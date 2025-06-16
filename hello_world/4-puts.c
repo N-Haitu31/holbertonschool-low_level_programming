cd1b96d50fe497f0d1d2594d6283e5bfc66d4bc8
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-16 16:15:36 +0200

    C program prints specific phrase using puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * print with puts "Programming is like building a multilingual puzzl
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 
-    return (0);
+	return (0);
 }

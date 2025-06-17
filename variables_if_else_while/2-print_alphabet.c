4a951402a26cd7866989619f488902e4938aaf84
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-17 21:51:10 +0200

    Print lowercase alphabet using putchar (twice max)

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints the alphabet in lowercase,
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	/*declare a variable*/
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+	{
+		/*print the value of the variable*/
+
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+}

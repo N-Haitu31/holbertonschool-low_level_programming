2dd26900d59076dff435c57cdc8b53a62561c6df
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-17 10:18:43 +0200

    Print specific message to stderr

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -10,7 +10,7 @@
 
 int main(void)
 {
-	write(stderr, "and that piece of art is useful\" -
+	write(2, "and that piece of art is useful\" -
 			Dora Korpar, 2015-10-19\n", "string format", 59);
 
 	return (1);

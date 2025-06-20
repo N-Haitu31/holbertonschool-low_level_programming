6a187e2056e56a2c10eebdea97eaae902689e975
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-20 10:18:39 +0200

    alphabetx10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+
+	for (i = 0; i <= 9; i++)
+
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+
+	}
+}		

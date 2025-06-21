932033f22b5a91cc7a6e13d1cc217257b75d3b5d
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-21 18:51:03 +0200

    the day of Jack Bauer, starting from 00:00 to 23:59

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,14 +9,24 @@ void jack_bauer(void)
 {
 	int heure;
 	int minute;
-
+	
+	/*Boucle externe pour les heures : itère de 00 à 23*/
 	for (heure = 0; heure < 24; heure++)
+
 	{
+		/*Boucle interne pour les minutes : itère de 00 à 59 pour chaque heure*/
 		for (minute = 0; minute < 60; minute++)
 		{
+			/*Affiche le chiffre des dizaines de l'heure*/
+			/*(ex: pour 05, heure / 10 = 0 ; pour 15, heure / 10 = 1)*/
+			/*Affiche le chiffre des unités de l'heure*/
+			/*(ex: pour 05, heure % 10 = 5 ; pour 15, heure % 10 = 5)*/
 			_putchar((heure / 10) + '0');
 			_putchar((heure % 10) + '0');
+
 			_putchar(':');
+
+			/*Même principe que pour les heures mais pour les minutes*/
 			_putchar((minute / 10) + '0');
 			_putchar((minute % 10) + '0');
 			_putchar('\n');

13f19dcbd0b48dbd935aa9937b3bc455ba7dfb7c
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-23 14:44:33 +0200

    Implement more_numbers function to print numbers from 0 to 14, ten times

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - to print 10 times from 0 to 14
+ *
+ * Return: 10 times frokm 0 to 14 and followed by new line
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+
+			}
+				_putchar(j % 10 + '0');
+
+		}
+
+	_putchar('\n');
+
+	}
+}

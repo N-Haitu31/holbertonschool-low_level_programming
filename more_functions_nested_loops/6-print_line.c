3cf26a17b386f0af7dfbedc7701f4486eceebbfc
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-23 15:30:11 +0200

    Implement print_line function to draw a straight line

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line n char.
+ * @n: the number of "_" to print
+ */
+
+void print_line(int n)
+
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}

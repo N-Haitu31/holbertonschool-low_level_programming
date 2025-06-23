b126ab893d85005d843df6f098e9466a9e40fbf1
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-23 12:57:28 +0200

    Implement print_numbers with strict putchar limit

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/**
+ /**
  * print_numbers - Prints the numbers from 0 to 9.
  * Description: This function uses _putchar to
  * print the numbers.
@@ -14,7 +14,7 @@ void print_numbers(void)
 	for (i = 0; i < 10; i++)
 
 		_putchar(i + '0');
-	
+
 	_putchar('\n');
 
 }

5ca8dd7b68732a43f718e11308ec46f9a48c9cf4
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-25 23:12:21 +0200

    Function that prints half of a string

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -23,16 +23,22 @@ void puts_half(char *str)
 	{
 		n = len / 2; /*Si 'len' est pair,*/
 			     /* 'n' est défini comme la moitié de la longueur*/
+	/*(Ex: si len=6, n=3). Cela représente l'index de début de la deuxième moitié*/
 
 	}
 
 	else
 	{
 		n = (len + 1) / 2; /*Longueur impaire*/
+	/*'n' est défini comme la moitié supérieure de la longueur*/
+	/*(Ex: si len=5, (5+1)/2 = 3. Si len=7, (7+1)/2 = 4)*/
+	/*l'index de début de la deuxième moitié (incluant le caractère du milieu)*/
 
 	}
 
-	while (n < i)
+	while (n < i) /*la valeur de 'n' (notre index de départ pour l'impression)*/
+	/*est inférieure à la valeur actuelle de 'i' (qui est égale à 'len' ici)*/
+	/*la boucle imprime les caractères de l'index 'n' jusqu'à 'len - 1'*/		
 	{
 		_putchar(str[n]);
 		n++;

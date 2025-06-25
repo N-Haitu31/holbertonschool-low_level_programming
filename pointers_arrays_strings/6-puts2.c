f692ec78c21b6b7f5f823d93d2f66ee1714c9e99
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-25 19:15:52 +0200

    Implement function to print every other character from a string

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints out every other char of the string.
+ * @str: input string to print.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	/* Boucle pour parcourir la chaîne tant que le caractère actuel*/
+	/* n'est pas le caractère nul*/
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0) /*Vérifie si l'index est pair*/
+		{
+			_putchar(str[i]);/*Imprime le caractère à l'index pair*/
+		}
+
+		i++;/*Incrémentation de l'index pour passer au caractère suivant*/
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -11,3 +11,5 @@ C - Pointers, arrays and strings
 4-print_rev.c : Write a function that prints a string, in reverse, followed by a new line.
 
 5-rev_string.c : Write a function that reverses a string.
+
+6-puts2.c : Write a function that prints every other character of a string, starting with the first character, followed by a new line.

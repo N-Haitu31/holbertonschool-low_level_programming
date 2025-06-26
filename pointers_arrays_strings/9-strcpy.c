ae023ce76361a2fe9f63efbac16c3a26bdcfd633
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-26 19:02:05 +0200

    Add _strcpy function to copy string with null terminator

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -9,14 +9,20 @@
 char *_strcpy(char *dest, char *src)
 
 {
-	size_t i;
+	size_t i;/*size_t: variable pour indice ou les tailles*/
 
 	for (i = 0 ; src[i] != '\0' ; i++)
+	/*Parcourir src tant que vous ne rencontrez pas la fin de la chaîne*/
 	{
 		dest[i] = src[i];
+	/*Copie les caractères de la chaîne source dans la destination avec i*/
 	}
 
 	dest[i] = '\0';
+	/*indique la fin de la chaîne dans dest, ce qui est cruciale*/
+	/* pour que d'autres fonctions puissent savoir où s'arrête la chaîne*/
 
 	return (dest);
+	/*retourne le pointeur vers dest, ce qui permet à l'utilisateur*/
+	/* de la fonction d'obtenir la chaîne copiée*/
 }

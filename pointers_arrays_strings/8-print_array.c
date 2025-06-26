1f8de701e5116a1ae9522e21fadaaa06116ece36
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-26 18:04:26 +0200

    Function that prints n elements of an array of integers

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,10 +13,14 @@ void print_array(int *a, int n)
 	if (n > 0)
 	{
 		for (i = 0; i <= n - 2; i++)
+/*'n-2'? parce que si on prend le dernier car. du tableau (-1024) -> (n-1)*/
+/*on ne veut pas ", " en sortie donc on décale pour prendre l'avt derniere valeur*/
 		{
 			printf("%d, ", a[i]);
 		}
-		printf("%d", a[n - 1]);
+/*ici on affiche '-1024' sans ", " en sortant du 'for' en affichant*/
+/*le dernier car. du tableau 'n-1'*/
+		printf("%d", a[n - 1])' 
 	}
 	printf("\n");
 }

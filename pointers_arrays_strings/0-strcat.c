1732f17741e53feef03c81db30900c38130b5df3
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-06-27 19:11:23 +0200

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,22 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0; /*Initialiser un index i à 0 pour parcourir la chaîne dest*/
+	int j = 0; /*Initialiser un index j à 0 pour parcourir la chaîne src*/
 
-	while (dest[i] != '\0')
+	while (dest[i] != '\0') /*Boucle pour trouver la fin de la chaîne dest*/
 	{
-		i++;
+		i++; /*Incrémenter i jusqu'à atteindre le caractère nul, ce qui marque la fin de dest*/
 	}
 
-	while (src[j] != '\0')
+	while (src[j] != '\0') /*Boucle pour copier les caractères de src vers dest*/
 	{
-		dest[i] = src[j];
+		dest[i] = src[j]; /*Copier le caractère actuel de src dans dest à la position finde*/
 
-		i++;
-		j++;
+		i++; /*Passer à la position suivante dans dest*/
+		j++; /*Passer à la position suivante dans src*/
 
 	}
-	dest[i] = '\0';
-	return (dest);
+	dest[i] = '\0'; /*Ajouter le caractère nul à la fin de dest pour s'assurer que c'est une chaîne valide*/
+	return (dest); /*Retourner le pointeur vers dest*/
 }

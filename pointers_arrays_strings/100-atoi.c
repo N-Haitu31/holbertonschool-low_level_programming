4099fe894ebf84180520cf448d5a1bdeda2cc16c
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-27 09:24:11 +0200

    Convert a string to an integer

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,12 +13,7 @@ int _atoi(char *s)
 	int i = 0;
 
 
-	while (s[i] == ' ')
-	{
-		i++;
-	}
-
-	while (s[i] == '+' || s[i] == '-')
+	while ((s[i] < '0' || s[i] > '9') && s[i] != '\0')
 	{
 		if (s[i] == '-')
 		{

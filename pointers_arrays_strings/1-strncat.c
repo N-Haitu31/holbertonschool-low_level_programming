808ae29049810c0a2b0f3ebb9aa4c6f3c1b9fa42
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-06-27 18:29:01 +0200

    1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -10,20 +10,23 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i = 0;
-	int j = 0;
+	int i = 0; /*Initialiser un index i à 0 pour parcourir la chaîne dest*/
+	int j = 0; /*Initialiser un index j à 0 pour parcourir la chaîne src*/
 
+	/*Boucle pour trouver la fin de la chaîne dest*/
 	while (dest[i] != '\0')
 	{
-		i++;
+		i++; /*Incrémenter i jusqu'à la fin de dest*/
 	}
 
+	/*Boucle pour copier les caractères de src vers dest*/
+	/*jusqu'à n caractères ou jusqu'à atteindre la fin de src*/
 	while ((j < n) && src[j] != '\0')
 	{
-		dest[i + j] = src[j];
+		dest[i + j] = src[j]; /*Copier le caractère actuel de src dans dest*/
 		j++;
 	}
 
-	dest[i + j] = '\0';
-	return (dest);
+	dest[i + j] = '\0'; /*Ajouter le caractère nul à la fin de dest pour terminer la chaîne*/
+	return (dest); /*Retourner le pointeur vers dest*/
 }

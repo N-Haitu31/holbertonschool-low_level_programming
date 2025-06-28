54509217ec1cce4a776b7b02e0f7890b1426734b
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-28 22:00:35 +0200

    Function that changes all lowercase letters of a string to uppercase

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -6,20 +6,22 @@
  * Return: the converted string.
  */
 
-char *string_toupper(char *str)
+char *string_toupper(char *str) /*Déclaration de la fonction qui*/
+			/* prend un pointeur vers une CHAINE DE CARACTERES*/
 
 {
 	int letter = 0;
 
-	while (str[letter] != '\0')
+	while (str[letter] != '\0') /*Boucle jusqu'à ce que l'on*/
+				    /*atteigne la fin de la chaîne*/
 	{
-		if  (str[letter] >= 97 && str[letter] <= 122)
+		if  (str[letter] >= 97 && str[letter] <= 122) /*Vérification si le caractère est une lettre minuscule (ASCII)*/
 		{
-			str[letter] = str[letter] - 32;
+			str[letter] = str[letter] - 32; /*Conversion du caractère en majuscule en soustrayant 32 cf. Tableau ASCII: ex: 'a' (97) et 'A' (65) : 97−65=32*/
 		}
 
 	letter++;
 	}
 
-	return (str);
+	return (str);/*Retourner la chaîne convertie en majuscules*/
 }

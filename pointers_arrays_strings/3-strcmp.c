9dda4c6dab9ee95a8ed1fc97d1311300d960e27f
N-Haitu31 <nguyenhaitu3@gmail.com>
2025-06-28 09:12:04 +0200

    Implement _strcmp function

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings, ending at the first byte that is different.
+ * @s1: string to be compared
+ * @s2: string to compare by
+ * Return: the difference between the first character that are not the same
+ */
+
+int _strcmp(char *s1, char *s2)
+
+{
+	int i = 0;
+
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+	i++;
+
+	}
+
+	if (s1[i] != '\0')
+	{
+		return (s1[i]);
+	}
+
+	else if (s2[i] != '\0')
+	{
+		return (-s2[i]);
+	}
+
+	return (0);
+}
--- pointers_arrays_strings/README.md
+++ pointers_arrays_strings/README.md
@@ -29,3 +29,5 @@ C - More pointers, arrays and strings
 1-strncat.c : Write a function that concatenates two strings
 
 2-strncpy.c : Write a function that copies a string.
+
+3-strcmp.c : Write a function that compares two strings.

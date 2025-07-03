6b3c1c7f0690cd437e95e794845eb0cb6879d99b
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-03 15:43:03 +0200

    Prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main  - main with parameters for number and command line arguments
+ * @argc : argument count
+ * @argv : argument vector
+ * Return: natural square root of number base
+ */
+
+int main(int argc, char *argv[])
+
+{
+	/*Ignore la variable argc car elle n'est pas utilisée ici*/
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

08d0ea345d62b630a38028c9f2ae64caacb0b31b
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-03 17:26:20 +0200

    Print a number, followed by a new line

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main  - main with parameters for number and command line arguments
+ * @argc : argument count
+ * @argv : argument vector
+ * Return: natural square root of number base
+ */
+
+int main(int argc, char *argv[])
+
+{
+	printf("%d\n", argc - 1);
+	(void)argv;
+	return (0);
+}

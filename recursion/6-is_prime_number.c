5c8a17b2901ae96a59c93cc4c6f2c144bf61650d
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-04 08:38:34 +0200

    Primenumber

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+  * is_prime_number_helper  - checks recursively the input from is_prime_number
+  * @n: iterator
+  * @divisor: base number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
+
 int is_prime_number_helper(int n, int divisor)
 
 {
@@ -21,6 +28,12 @@ int is_prime_number_helper(int n, int divisor)
 
 	return (is_prime_number_helper(n, divisor + 1));
 }
+
+/**
+  * is_prime_number - checks if the number is a prime number
+  * @n: the number to check
+  * Return: 1 if n is a prime, else return 0 otherwise.
+  */
 
 int is_prime_number(int n)
 

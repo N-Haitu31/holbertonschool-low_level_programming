0fb32260e4b6e2926dd859bb47268d4a3408d365
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-09 11:04:25 +0200

    Add _calloc function for array memory allocation

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * _calloc - allocates memory for an initialized buffer
+  * @nmemb: number of memory bytes
+  * @size: number of data type bits
+  * Return: return pointer to initialized buffer
+  */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+
+{
+	unsigned int i;
+	char *pointeur;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	pointeur = malloc(nmemb * size);
+
+	if (pointeur == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		((char *) pointeur)[i] = 0;
+	}
+
+	return (pointeur);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -1,7 +1,15 @@
 ===== M O R E  M A L L O C,  F R E E ======
 
 0-malloc_checked.c - 0. Trust no one
+
 Write a function that allocates memory using malloc.
 
 1-string_nconcat.c - 1. string_nconcat
+
 Write a function that concatenates two strings. 
+
+2-calloc.c - 2. _calloc
+
+Write a function that allocates memory for an array, using malloc.
+
+

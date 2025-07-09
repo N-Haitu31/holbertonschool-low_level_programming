1ba764cdf2ff134cdeffe379b434a3510fcaf08f
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-09 11:59:59 +0200

    Add _calloc function for array memory allocation

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 {
 	unsigned int i;
-	unsigned int *pointeur;
+	char  *pointeur;
 
 	if (nmemb == 0 || size == 0)
 	{
@@ -27,7 +27,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	}
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		pointeur[i] = 0;
 	}

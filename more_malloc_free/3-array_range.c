ce59e438b12a6cb3cc9aca53b547718e7bcce7fb
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-09 13:41:10 +0200

    Create function to generate integer array

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -15,11 +15,14 @@ int *array_range(int min, int max)
 	int i, taille;
 	int *array;
 
-	if (min > max)
+	if (min > max) /*Gérer le cas d'erreur où l'intervalle est invalide*/
 	{
 		return (NULL);
 	}
 
+	/*Calculer la taille nécessaire du tableau*/
+	/*La taille est déterminée par le nombre d'éléments de 'min' à 'max' inclus*/
+	/*exemple: de 2 à 5, il y a (5 - 2 + 1) = 4 éléments (2, 3, 4, 5)*/
 	taille = max - min + 1;
 
 	array = malloc(taille * sizeof(int));
@@ -31,6 +34,8 @@ int *array_range(int min, int max)
 
 	for (i = 0; i < taille; i++)
 	{
+		/*À chaque itération, elle assigne la valeur 'min + i' à l'élément courant du tableau*/
+		/*Ainsi, array[0] = min, array[1] = min + 1, ..., array[taille-1] = min + (taille-1) = max*/
 		array[i] = min + i;
 	}
 

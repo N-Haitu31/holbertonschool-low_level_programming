0dce07f3f78ac08b3fcec015e9f60d0821ae3c5c
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-09 12:07:20 +0200

    Add _calloc function for array memory allocation

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -13,20 +13,26 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 {
 	unsigned int i;
-	char  *pointeur;
+	char  *pointeur; /*'char' est garanti de faire 1 octet*/
+			 /*ce qui nous permet d'accéder et d'initialiser chaque octet individuellement*/
 
-	if (nmemb == 0 || size == 0)
+	if (nmemb == 0 || size == 0) /*Gérer les cas d'entrée invalides*/
 	{
 		return (NULL);
 	}
 
+	/*Le type de retour de malloc est 'void *', qui est implicitement converti*/
+	/*en 'char *' pour notre pointeur en C*/
 	pointeur = malloc(nmemb * size);
 
-	if (pointeur == NULL)
+	if (pointeur == NULL) /*Vérifier si l'allocation mémoire a échoué*/
 	{
 		return (NULL);
 	}
 
+	/*Initialiser tous les octets alloués à zéro*/
+	/*Elle commence à l'index 0 et va jusqu'à (nmemb * size) - 1*/
+	/*Grâce au 'char *pointeur', 'pointeur[i]' accède précisément à un octet à la fois*/
 	for (i = 0; i < (nmemb * size); i++)
 	{
 		pointeur[i] = 0;

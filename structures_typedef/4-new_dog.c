9076f50c7ecc1ede8d1cad5c37603e77ab80cfb4
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-11 12:21:20 +0200

    Create new dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,43 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * new_dog - creates a new struct of type dog
+  * @name: struct parameter name
+  * @age: struct parameter age
+  * @owner: struct parameter owner
+  * Return: returns pointer my_dog
+  */
+
+dog_t *new_dog(char *name, float age, char *owner)
+
+{
+	dog_t *my_dog;
+
+	my_dog = malloc(sizeof(dog_t));
+
+	if (my_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	my_dog->name = strdup(name);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+
+	my_dog->age = age;
+
+	my_dog->owner = strdup(owner);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+	return (my_dog);
+}
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -11,3 +11,6 @@ Write a function that prints a struct dog.
 
 3-dog.h - 3. Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read
 Define a new type dog_t as a new name for the type struct dog.
+
+4-new_dog.c - 4. A door is what a dog is perpetually on the wrong side of
+Write a function that creates a new dog.

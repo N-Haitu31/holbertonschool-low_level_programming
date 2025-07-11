48ad4424c8cea71fee85c20fa45a0927c5fdd4f8
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-11 12:48:37 +0200

    Create new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -17,27 +17,38 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *my_dog;
 
 	my_dog = malloc(sizeof(dog_t));
-
 	if (my_dog == NULL)
-	{
 		return (NULL);
-	}
 
-	my_dog->name = strdup(name);
-	if (my_dog->name == NULL)
+	if (name == NULL)
+		my_dog->name = NULL;
+
+	else
 	{
-		free(my_dog);
-		return (NULL);
+		my_dog->name = strdup(name);
+		if (my_dog->name == NULL)
+		{
+			free(my_dog);
+			return (NULL);
+		}
 	}
 
 	my_dog->age = age;
+	if (owner == NULL)
+		my_dog->owner = NULL;
 
-	my_dog->owner = strdup(owner);
-	if (my_dog->owner == NULL)
+	else
 	{
-		free(my_dog->name);
-		free(my_dog);
-		return (NULL);
+		my_dog->owner = strdup(owner);
+		if (my_dog->owner == NULL)
+		{
+			if (my_dog->name != NULL)
+			{
+				free(my_dog->name);
+			}
+			free(my_dog);
+			return (NULL);
+		}
 	}
 	return (my_dog);
 }

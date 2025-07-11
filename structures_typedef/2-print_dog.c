92b7b29d4202324e221e84fbf4e374f194a00c52
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-11 09:58:07 +0200

    Add function to print struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,14 +9,19 @@
 
 void print_dog(struct dog *d)
 {
+	/*--- 1. Vérification du pointeur de la structure principale ---*/
+	/*Si le pointeur 'd' lui-même est NULL,*/
+	/*cela signifie qu'aucune structure 'dog' valide n'a été passée à la fonction*/
 	if (d == NULL)
 	{
-		return;
+		return; /*Puisque c'est une fonction 'void' (qui ne retourne aucune valeur)*/
 	}
 
+	/*Le champ 'name' est un 'char *', c'est-à-dire un pointeur vers une chaîne de caractères. D'où le 'd->name'*/
 	if (d->name == NULL)
 	{
-		printf("Name: (nil)\n");
+		printf("Name: (nil)\n"); /*À la place, on affiche une chaîne de caractères indiquant que le nom est "null"*/
+					 /*ou non défini, ici "(nil)", ce qui est une convention courante.*/
 	}
 
 	else

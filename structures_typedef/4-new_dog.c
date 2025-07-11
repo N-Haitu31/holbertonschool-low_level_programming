38c6015abfeafdb9a14378cc7a2ccb7c8073202f
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-11 17:31:10 +0200

    Create new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -4,51 +4,88 @@
 #include <string.h>
 
 /**
-  * new_dog - creates a new struct of type dog
-  * @name: struct parameter name
-  * @age: struct parameter age
-  * @owner: struct parameter owner
-  * Return: returns pointer my_dog
-  */
+ * _strcpy - Copie la chaîne source dans la destination.
+ * @dest: Le pointeur vers la chaîne de destination où copier.
+ * @src: Le pointeur vers la chaîne source à copier.
+ *
+ * Return: Le pointeur vers la chaîne de destination (dest).
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
+ * _strlen - Calcule la longueur d'une chaîne de caractères.
+ * @str: Le pointeur vers la chaîne dont la longueur doit être calculée.
+ * Return: La longueur de la chaîne (nbre de car.
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+
+}
+
+/**
+ * new_dog - Crée une nouvelle structure 'dog' et l'initialise.
+ * @name: Le nom du chien.
+ * @age: L'âge du chien.
+ * @owner: Le nom du propriétaire du chien.
+ * Return: pointer to new_dog
+ */
 
 dog_t *new_dog(char *name, float age, char *owner)
-
 {
 	dog_t *my_dog;
+	int name_len;
+	int owner_len;
+
+	if (name == NULL || owner == NULL || age < 0)
+	{
+		return (NULL);
+	}
+
+	name_len = _strlen(name) + 1;
+	owner_len = _strlen(owner) + 1;
 
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
+	{
 		return (NULL);
-
-	if (name == NULL)
-		my_dog->name = NULL;
-
-	else
-	{
-		my_dog->name = strdup(name);
-		if (my_dog->name == NULL)
-		{
-			free(my_dog);
-			return (NULL);
-		}
 	}
 
+	my_dog->name = malloc(name_len);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+
+	_strcpy(my_dog->name, name);
+	my_dog->owner = malloc(owner_len);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog->name);
+		free(my_dog);
+		return (NULL);
+	}
+
+	_strcpy(my_dog->owner, owner);
 	my_dog->age = age;
-	if (owner == NULL)
-		my_dog->owner = NULL;
-
-	else
-	{
-		my_dog->owner = strdup(owner);
-		if (my_dog->owner == NULL)
-		{
-			if (my_dog->name != NULL)
-			{
-				free(my_dog->name);
-			}
-			free(my_dog);
-			return (NULL);
-		}
-	}
 	return (my_dog);
 }

7090c74642d26669ab17cf70f52dbc01230088d4
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-16 15:30:13 +0200

    Add function to print numbers followed by newline

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+  * print_numbers - prints numbers
+  * @separator: character to use to delimit
+  * @n: number of elements to print
+  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	int is_first = 1;
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		int number = va_arg(args, int);
+
+		if (is_first)
+		{
+			printf("%d", number);
+			is_first = 0;
+		}
+
+		else
+		{
+			printf("%s%d", separator, number);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
--- variadic_functions/README.md
+++ variadic_functions/README.md
@@ -3,3 +3,6 @@
 
 0-sum_them_all.c - 0. Beauty is variable, ugliness is constant
 Write a function that returns the sum of all its parameters
+
+1-print_numbers.c - 1. To be is to be the value of a variable
+Write a function that prints numbers, followed by a new line.

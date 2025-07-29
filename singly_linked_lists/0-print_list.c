49a98bb1badc8b39f61a56d6d5c9a1eeaead4fd7
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-29 15:43:00 +0200

    feat: Add print_list function to display list_t elements

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+* print_list - prints all elements of a list_t list
+* @h: singly linked list to print
+* Return: number of nodes in the list
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0](nil)\n");
+		}
+		else
+		{
+			printf("[%d]%s\n", current->len, current->str);
+		}
+		count++;
+		current = current->next;
+	}
+	return (count);
+}

8389b99093a68e84046a527de37ae306f6972a23
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-31 18:00:51 +0200

    fix: implement function because code fail

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,48 +4,52 @@
 #include "lists.h"
 
 /**
-* insert_dnodeint_at_index - Inserts a new node at a given position
-* @h: Double pointer to the head of the list
-* @idx: Index at which the new node should be added (starting from 0)
-* @n: Value to store in the new node
-*
-* Return: Address of the new node, or NULL if it failed
-*/
+ * insert_dnodeint_at_index - Inserts a new node at a given position
+ * @h: Double pointer to the head of the list
+ * @idx: Index at which the new node should be added (starting from 0)
+ * @n: Value to store in the new node
+ *
+ * Return: Address of the new node, or NULL if it failed
+ */
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *newnode;
-	dlistint_t *node = *h;
+	unsigned int count = 0;
+	dlistint_t *current = *h;
+	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (idx == 0)
 	{
 		return (add_dnodeint(h, n));
 	}
-	while (node != NULL && idx > 0)
+
+	while (current != NULL && count < idx)
 	{
-		idx--;
-		node = node->next;
+		count++;
+		current = current->next;
 	}
-	if (node == NULL && idx > 0)
-	{
-		return (NULL);
-	}
-	else if (node == NULL)
+
+	if (current == NULL && count == idx)
 	{
 		return (add_dnodeint_end(h, n));
 	}
-	else
+
+	else if (count < idx)
 	{
-		newnode = malloc(sizeof(dlistint_t));
-		if (newnode == NULL)
-		{
-			return (NULL);
-		}
-		newnode->n = n;
-		newnode->next = node;
-		newnode->prev = node->prev;
-		node->prev->next =  newnode;
-		node->prev = newnode;
+		return (NULL);
 	}
-	return (newnode);
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->n = n;
+	new_node->prev = current->prev;
+	new_node->next = current;
+
+	new_node->prev->next = new_node;
+	new_node->next->prev = new_node;
+
+	return (new_node);
 }

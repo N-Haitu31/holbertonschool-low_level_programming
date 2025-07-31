721a56c4bad698dd4fe49a2a202a91ac8ce53eca
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-07-31 18:02:20 +0200

    fix: implement function because code fail

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -4,13 +4,13 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position
- * @h: Double pointer to the head of the list
- * @idx: Index at which the new node should be added (starting from 0)
- * @n: Value to store in the new node
- *
- * Return: Address of the new node, or NULL if it failed
- */
+* insert_dnodeint_at_index - Inserts a new node at a given position
+* @h: Double pointer to the head of the list
+* @idx: Index at which the new node should be added (starting from 0)
+* @n: Value to store in the new node
+*
+* Return: Address of the new node, or NULL if it failed
+*/
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {

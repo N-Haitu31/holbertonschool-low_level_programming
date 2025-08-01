1f261754ee1d25a5c0bf5e82abb4bc8f5254d046
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-08-01 22:05:59 +0200

    fix requirement fail

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -6,11 +6,11 @@
 int calculate_length(char *text_content);
 
 /**
- * append_text_to_file - appends text to a file
- * @filename: file to append to, if NULL, return -1
- * @text_content: content to append, if NULL, do not append
- * Return: 1 on success, -1 on failure
- */
+* append_text_to_file - appends text to a file
+* @filename: file to append to, if NULL, return -1
+* @text_content: content to append, if NULL, do not append
+* Return: 1 on success, -1 on failure
+*/
 
 int append_text_to_file(const char *filename, char *text_content)
 {
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		return (-1);
+		return (1);
 	}
 
 	fd = open(filename, O_WRONLY | O_APPEND);

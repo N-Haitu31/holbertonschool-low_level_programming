ed461fbfc8734880d1213afbba2495017edb810b
N-Haitu31 <nguyenhaitu31@gmail.com>
2025-08-02 13:36:22 +0200

    fix: Add loop for size

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,19 +3,15 @@
 #include <stdio.h>
 
 /**
-* hash_table_create - creates a hash table.
-* @size: size of the hash table.
-* Return: pointer to the hash table
-*/
+ * hash_table_create - creates a hash table.
+ * @size: size of the hash table.
+ * Return: pointer to the hash table
+ */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_t_create;
-
-	if (size == 0)
-	{
-		return (NULL);
-	}
+	unsigned long int i;
 
 	h_t_create = malloc(sizeof(hash_table_t));
 
@@ -26,12 +22,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	h_t_create->size = size;
 
-	h_t_create->array = calloc(size, sizeof(hash_node_t *));
+	h_t_create->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (h_t_create->array == NULL)
 	{
-		free(h_t_create);
 		return (NULL);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		h_t_create->array[i] = NULL;
 	}
 
 	return (h_t_create);
